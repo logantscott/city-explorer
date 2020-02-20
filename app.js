f3b04bcad4a7148579b69f7f4ca81d6aeb449183
Logan Scott <logantscott@gmail.com>
2020-02-20 12:18:50 -0800

    travis4

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const app = express();
 const cors = require('cors');
 const request = require('superagent');
 
-const { GEOCODE_API_KEY,
-    WEATHER_API_KEY,
-    YELP_API_KEY,
-    TRAIL_API_KEY,
-    EVENT_API_KEY } = process.env;
+// const { GEOCODE_API_KEY,
+//     WEATHER_API_KEY,
+//     YELP_API_KEY,
+//     TRAIL_API_KEY,
+//     EVENT_API_KEY } = process.env;
 
 let lat;
 let lng;
